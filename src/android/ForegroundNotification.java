0077b22e4de998163631e20cd0cd509949393e40
Zhengz <zhengz@masn.cn>
2015-08-05 14:23:17 +0800

    add android action

--- src/android/ForegroundNotification.java
+++ src/android/ForegroundNotification.java
@@ -1,8 +1,12 @@
 package com.mobishift.plugins.foregroundnotification;
 
+import android.content.Intent;
+import android.util.Log;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,22 +15,53 @@ import org.json.JSONObject;
  * This class echoes a string called from JavaScript.
  */
 public class ForegroundNotification extends CordovaPlugin {
+    private static final String TAG = "ForegroundNotification";
+    private static final String ACTION_START = "start";
+    private static final String ACTION_CANCEL = "cancel";
+    private static final String ACTION_GETURL = "getURL";
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
-        return false;
+    private static String urlPath = null;
+    private static CallbackContext callbackContext = null;
+    public static void setUrlPath(String url){
+
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+    private Intent serviceIntent;
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        boolean result = false;
+        if(action.equals(ACTION_START)){
+
+        }else if(action.equals(ACTION_CANCEL)){
+
+        }else if(action.equals(ACTION_GETURL)){
+            ForegroundNotification.callbackContext = callbackContext;
+        }
+
+        return result;
+    }
+
+    private void start(){
+
+    }
+
+    private void cancel(){
+
+    }
+
+    private static void getURL(){
+        if(callbackContext != null && urlPath != null){
+            JSONObject jsonObject = new JSONObject();
+            try{
+                jsonObject.put("urlPath", urlPath);
+            }catch (JSONException ex){
+                Log.e(TAG, ex.getMessage());
+            }
+            urlPath = null;
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, jsonObject);
+            pluginResult.setKeepCallback(true);
+            callbackContext.sendPluginResult(pluginResult);
+
         }
     }
 }
