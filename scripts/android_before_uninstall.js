da088e3816258a041cd539939803671ebc013506
Zhengz <zhengz@masn.cn>
2015-08-06 11:58:24 +0800

    fix hook

--- scripts/android_before_uninstall.js
+++ scripts/android_before_uninstall.js
@@ -13,8 +13,8 @@ module.exports = function(context){
     var content = fs.readFileSync(targetFile, {encoding: 'utf8'});
     if(content.indexOf('com.mobishift.plugins.foregroundnotification.ForegroundNotification') !== -1){
         content = content.replace('import com.mobishift.plugins.foregroundnotification.ForegroundNotification;', '')
-                        .replace('if(getIntent().hasExtra("foregroundUrl")){ForegroundNotification.setUrlPath(getIntent().getStringExtra("foregroundUrl"));}')
-                        .replace('if(intent.hasExtra("foregroundUrl")){ForegroundNotification.setUrlPath(intent.getStringExtra("foregroundUrl"));}');
+                        .replace('if(getIntent().hasExtra("foregroundUrl")){ForegroundNotification.setUrlPath(getIntent().getStringExtra("foregroundUrl"));}', '')
+                        .replace('if(intent.hasExtra("foregroundUrl")){ForegroundNotification.setUrlPath(intent.getStringExtra("foregroundUrl"));}', '');
         
         fs.writeFileSync(targetFile, content);
     }
